6a2939c1c0680473db8339927d45fd494c634886
shanzedur rahim <shanz.rahim@gmail.com>
2018-10-25 22:38:14 -0400

    done

---
+++ src/base/confi1.java
@@ -0,0 +1,15 @@
+//package base;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//public class confi1 {
+//
+//	public static void main(String[] args) {
+////		
+////		System.setProperty("webdriver.chrome.driver","/Users/shanzedurrahim/Documents/github/SeleniumFramework/Drivers/chromedriver");
+////		WebDriver driver= new ChromeDriver();
+////		driver.get("https://www.facebook.com/");
+//	}
+//
+//}
